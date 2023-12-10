82e9eb8e13dcbfef6bafbffa00bd01c644f7b31e
pmAdriaan <pmAdriaan@users.noreply.github.com>
2023-12-10 09:19:42 +0000

    Save/load tasks from local storage and code refactor

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
+    const datePicker = $('#datepicker');
+    const container = $('.container');
+    const confirmMessage = $('#confirm-message');
+    const dateFormat = 'MM/DD/YYYY';
+
     // Initialize jQuery UI calendar
-    $('#datepicker').datepicker({
-        onSelect: function (dateText) {
-            // Update time block styles based on the selected date
-            currentDate(dayjs(dateText));
-            updateTimeBlockStyles(dayjs(dateText));
-        },
+    datePicker.datepicker({
+        onSelect: handleDateSelection,
         showOtherMonths: true,
         selectOtherMonths: true,
         changeMonth: true,
@@ -14,22 +15,28 @@ $(document).ready(function () {
         firstDay: 1,
     });
 
+    function handleDateSelection(dateText) {
+        const selectedDate = dayjs(dateText);
+        currentDate(selectedDate);
+        updateTimeBlockStyles(selectedDate);
+        loadTasks(selectedDate.format(dateFormat));
+    }
+
     // Function to update the current date
     function currentDate(date) {
-        let formattedDate = date.format('dddd, MMMM D[th], YYYY');
+        const formattedDate = date.format('dddd, MMMM D[th], YYYY');
         $('#currentDay').text('Date: ' + formattedDate);
     }
 
     // Function to generate time blocks
     function generateTimeBlocks() {
-        let container = $('.container');
-        let businessHours = ['9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM'];
+        const businessHours = ['9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM'];
 
-        $.each(businessHours, function (_, hour) {
-            let timeBlock = $('<div>').addClass('row time-block');
-            let timeElement = $('<div>').addClass('col-2 hour').text(hour);
-            let taskInput = $('<textarea>').addClass('col description task-input');
-            let saveBtn = $('<button>').addClass('col-1 saveBtn').html('<i class="fas fa-save"> Save</i>');
+        businessHours.forEach(hour => {
+            const timeBlock = $('<div>').addClass('row time-block');
+            const timeElement = $('<div>').addClass('col-2 hour').text(hour);
+            const taskInput = $('<textarea>').addClass('col description task-input');
+            const saveBtn = $('<button>').addClass('col-1 saveBtn').html('<i class="fa-regular fa-floppy-disk"></i> Save');
 
             timeBlock.append(timeElement, taskInput, saveBtn);
             container.append(timeBlock);
@@ -46,7 +53,7 @@ $(document).ready(function () {
             }
 
             // Combine selected date with blockHour to get a date time for comparison
-            let blockDateTime = selectedDate.hour(blockHour);
+            const blockDateTime = selectedDate.hour(blockHour);
 
             if (blockDateTime.isAfter(dayjs())) {
                 $(this).addClass('future').removeClass('present past');
@@ -58,16 +65,56 @@ $(document).ready(function () {
         });
     }
 
+    // Save tasks to local storage
+    function saveTasks(date, tasks) {
+        localStorage.setItem(date, JSON.stringify(tasks));
+    }
+
+    // Load tasks from local storage
+    function loadTasks(date) {
+        let tasks = JSON.parse(localStorage.getItem(date)) || [];
+        $('.time-block').each(function (index) {
+            $(this).find('.task-input').val(tasks[index] || '');
+        });
+    }
+
     // Call the function to generate time blocks
     generateTimeBlocks();
 
     // Show today's date when the page is loaded
-    let today = dayjs();
+    const today = dayjs();
     currentDate(today);
     updateTimeBlockStyles(today);
 
+    // Set the default date for the datepicker
+    datePicker.datepicker('setDate', today.toDate());
+
+    // Load tasks for today
+    loadTasks(today.format(dateFormat));
+
     // Update time block styles when the user scrolls
     $(window).scroll(function () {
-        updateTimeBlockStyles(dayjs($('#datepicker').datepicker('getDate')));
+        updateTimeBlockStyles(dayjs(datePicker.datepicker('getDate')));
+    });
+
+    // Save tasks when the save button is clicked
+    container.on('click', '.saveBtn', function () {
+        const selectedDate = dayjs(datePicker.datepicker('getDate')).format(dateFormat);
+
+        let tasks = [];
+
+        $('.time-block').each(function () {
+            tasks.push($(this).find('.task-input').val());
+        });
+
+        saveTasks(selectedDate, tasks);
+
+        // Show confirmation message with icon
+        confirmMessage.append('<p id="confirm"><i class="fa-solid fa-clipboard-check"></i>Task added to localStorage!</p>');
+
+        // Set a timeout to clear the confirmation message after 3 seconds
+        setTimeout(function () {
+            confirmMessage.text('');
+        }, 3000);
     });
 });
